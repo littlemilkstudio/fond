2fe9a20352d842128496eea1b75087f5b4a0b3c3
Maxwell Yinger <max@bramble.studio>
2018-01-29 15:30:53 -0700

    docker integration

---
+++ wordpress/wp-content/themes/fond/page-homepage.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Template Name: Homepage
+ * The template for displaying the homepage.
+ *
+ *
+ * @package fond
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main home" role="main">
+			
+			<!-- THIS IS A TEST -->
+			<h1>Lets see if this is working?</h1>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php get_footer(); ?>
---
+++ wordpress/wp-content/themes/fond/src/components/app/index.js
@@ -0,0 +1,12 @@
+import $ from 'jquery';
+//import { Controller } from 'scrollmagic';
+//import Barba from 'barba.js';
+
+/*   Components    */
+//import '../navigation';
+
+
+$(document).ready( () => {
+
+	
+});
